21abb92e7e434c669fe7294aac7cfb73107c08e6
Aman Pradhan <amnapradhan99.ap@gmail.com>
2019-06-17 21:44:45 +0530

    Basic of List

---
+++ tuples.py
@@ -0,0 +1,21 @@
+Python 3.7.3 (v3.7.3:ef4ec6ed12, Mar 25 2019, 22:22:05) [MSC v.1916 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 	#	Tuples
+	
+>>> tuples = (1,2,3,4,6)	# tuples of integer
+>>> tuples
+(1, 2, 3, 4, 6)
+>>> name = ('Aman','Pradhan','Hello')	# Tuples of String
+>>> name
+('Aman', 'Pradhan', 'Hello')
+>>> name = ('Aman', 1, 'Pradhan', 76, 'Hello', 87)
+>>> mixed = ('Aman', 1, 'Pradhan', 76, 'Hello', 87)
+>>> mixed
+('Aman', 1, 'Pradhan', 76, 'Hello', 87)
+>>> mixed = ('Aman', 1, 'Pradhan', 76, 'Hello', 8.7)	# tuples of mixed data type
+>>> name = ('Aman', 1, 'Pradhan', 76, 'Hello', 87)
+>>> mixed
+('Aman', 1, 'Pradhan', 76, 'Hello', 8.7)
+>>> name[4]
+'Hello'
+>>> 
