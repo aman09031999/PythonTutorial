397c374740dfda9c9cb1f939e8745e5c46f36370
Aman Pradhan <amnapradhan99.ap@gmail.com>
2019-06-17 21:10:55 +0530

    Basic Arithmetic, String programming...

---
+++ basic.py
@@ -0,0 +1,49 @@
+Python 3.7.3 (v3.7.3:ef4ec6ed12, Mar 25 2019, 22:22:05) [MSC v.1916 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 3 + 9
+12
+>>> 3 * 8
+24
+>>> 2//3
+0
+>>> 4//6
+0
+>>> 4/6
+0.6666666666666666
+>>> 8 + 2 * 3
+14
+>>> (8 + 2) * 3
+30
+>>> 3 ** 2
+9
+>>> "Hello"
+'Hello'
+>>> 'hello'
+'hello'
+>>> print('Aman')
+Aman
+>>> print("Aman")
+Aman
+>>> print("Aman's Laptop")
+Aman's Laptop
+>>> print('Aman's "Laptop"')
+      
+SyntaxError: invalid syntax
+>>> print('Aman "Laptop"')
+Aman "Laptop"
+>>> print('Aman\'s "Laptop"')
+Aman's "Laptop"
+>>> 'Aman' + 'Pradhan'
+'AmanPradhan'
+>>> 'Aman' * 3
+'AmanAmanAman'
+>>> 'Aman' + 3
+Traceback (most recent call last):
+  File "<pyshell#18>", line 1, in <module>
+    'Aman' + 3
+TypeError: can only concatenate str (not "int") to str
+>>> print(r'C:\div\aman')
+C:\div\aman
+>>> print(r'C:\div\aman')	# r : means raw print
+C:\div\aman
+>>> 
